8b604d16232ca0f240c956ae5c58e7216a11d854
Попонин Юрий <juragamesaz@gmail.com>
2026-05-07 00:04:23 +0300

    Refactor sensor classes and update control logic

--- shedevroferma/shedevroferma.ino
+++ shedevroferma/shedevroferma.ino
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <DHT.h>
 
-class Thermometer {
-private:
-	int pin;
-public:
-	float temperature; //celcius
-  DHT dht;
-public:
-	Thermometer(int pin) : dht(pin, DHT11), pin(pin) { temperature = 0; } // конструктор, присваивает переменной начальное значение
-public:
-	void get_temperature() { temperature = dht.readTemperature(); }
-};
 
-class HygrometerAir {
+class AirSensor {
 private:
-	int pin;
-public:
-	int humidity;
+  int pin;
   DHT dht;
 public:
-	HygrometerAir(int pin) : dht(pin, DHT11), pin(pin) { humidity = 0; }
+  float humidity;
+  float temperature;
 public:
-	void get_humidity() { humidity = dht.readHumidity(); }
+  AirSensor(int pin): dht(pin, DHT11), pin(pin){
+    humidity = 0;
+    temperature = 0;
+  }
+public:
+  void begin() { dht.begin(); }
+public:
+  void update() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+    // Проверка: если датчик вернул ошибку (NaN), не обновляем переменные
+  if (!isnan(h) && !isnan(t)) {
+    humidity = h;
+    temperature = t;
+    }
+  }
 };
 
 class HygrometerSoil {
@@ -132,7 +136,7 @@ void Fan::power(){
 
 
 
-void control_temperature(Thermometer &t, Heater &h, Fan &f){
+void control_temperature(AirSensor &t, Heater &h, Fan &f){
   if(t.temperature > 22 && t.temperature < 27){
     h.on_heat = false;
     f.on_fan = false;
@@ -147,7 +151,7 @@ void control_temperature(Thermometer &t, Heater &h, Fan &f){
   }
 }
 
-void control_air_humidity(HygrometerAir &hum, Pump &p, Fan&f){
+void control_air_humidity(AirSensor &hum, Pump &p, Fan &f){
   if(hum.humidity > 1 && hum.humidity < 1){ //TODO
     p.on_pump = false;
     f.on_fan = false;
@@ -162,7 +166,7 @@ void control_air_humidity(HygrometerAir &hum, Pump &p, Fan&f){
   }
 }
 
-void control_soil_humidity(HygrometerSoil &hum, Pump &p, Fan&f){
+void control_soil_humidity(HygrometerSoil &hum, Pump &p, Fan &f){
   if(hum.humidity > 990 && hum.humidity < 1040){
     p.on_pump = false;
     f.on_fan = false;
@@ -178,7 +182,7 @@ void control_soil_humidity(HygrometerSoil &hum, Pump &p, Fan&f){
 }
 
 void control_light(Light &lig, Lamp&l){
-  if(lig.light < 500){ //TODO границы
+  if(lig.light < 500){
     l.on_lamp = false;
   }
   else{
@@ -192,222 +196,40 @@ Lamp lamp(6);
 Heater heater(4);
 Pump pump(5);
 
-//Thermometer thermometer();
-//HygrometerAir airHumidity();
-HygrometerSoil soilHumidity(A1);
+AirSensor air(2);
+HygrometerSoil soil(A1);
 Light light(A0);
 
 
 void setup(){
   Serial.begin(9600);
+  air.begin();
 }
+
+unsigned long lastUpdate = 0;
 
 void loop() {
-		//thermometer.get_temperature();
-		//airHumidity.get_humidity();
-    soilHumidity.get_humidity();
-		light.get_light();
-
-		//control_temperature(thermometer, heater, fan);
-		//control_air_humidity(airHumidity, pump, fan);
-    control_soil_humidity(soilHumidity, pump, fan);
-		control_light(light, lamp);
-
-		heater.power();
-		fan.power();
-		lamp.power();
-		pump.power();
-
-    Serial.print(" влажность почвы "); Serial.print(soilHumidity.humidity);
-    //Serial.print(" влажность воздуха "); Serial.print(airHumidity.humidity);
-    //Serial.print(" температура "); Serial.print(thermometer.temperature);
-    Serial.print(" освещенность "); Serial.println(light.light);
-}#include <stdio.h>
-
-
-class Thermometer {
-public:
-	int temperature; //celcius
-public:
-	Thermometer() { temperature = 0; } // конструктор, присваивает переменной начальное значение
-public:
-	void get_temperature() { temperature = analogRead(/**/); }
-};
-
-class Humidity {
-public:
-	int humidity;
-public:
-	Humidity() { humidity = 0; }
-public:
-	void get_humidity() { humidity = analogRead(/*TODO: PIN*/); }
-};
-
-class Light {
-public:
-	int light;
-public:
-	Light() { light = 0; }
-public:
-	void get_light() { light = analogRead(/**/); }
-};
-
-
-
-class Lamp {
-private:
-	int pin;
-public:
-	bool on_lamp;
-public:
-	Lamp(int pin) : pin(pin) { on_lamp = false; }; // присваивает лампе определенный пин и объявляет переменные
-public:
-	void power();
-};
-
-class Fan {
-private:
-  int pin;
-public:
-	bool on_fan;
-public:
-	Fan(int pin) : pin(pin) { on_fan = false; };
-public:
-	void power();
-};
-
-class Heater {
-private:
-  int pin;
-public:
-	bool on_heat;
-public:
-	Heater(int pin) : pin(pin) { on_heat = false; };
-public:
-	void power();
-};
-
-class Pump {
-private:
-  int pin;
-public:
-	bool on_pump;
-public:
-	Pump(int pin) : pin(pin) { on_pump = false; };
-public:
-	void power();
-};
-
-
-
-void Heater::power()
-{
-	if (on_heat) {digitalWrite(pin, HIGH);}
-	else {digitalWrite(pin, LOW);}
-}
-
-void Pump::power()
-{
-	if (on_pump) {digitalWrite(pin, HIGH);}
-	else {digitalWrite(pin, LOW);}
-}
-
-void Lamp::power()
-{
-	if (on_lamp) {digitalWrite(pin, HIGH);}
-	else {digitalWrite(pin, LOW);}
-}
-
-void Fan::power(){
-  if (on_fan) {digitalWrite(pin, HIGH);}
-	else {digitalWrite(pin, LOW);}
-}
-
-
-
-void control_temperature(Thermometer &t, Heater &h, Fan &f){
-  if(t.temperature > 22 && t.temperature < 27){
-    h.on_heat = false;
-    f.on_fan = false;
-  } 
-  else if(t.temperature < 20){
-    h.on_heat = true;
-    f.on_fan = false;
+  if(millis() - lastUpdate >= 2000){
+    lastUpdate = millis();
+    air.update();
   }
-  else if(t.temperature > 30){
-    h.on_heat = false;
-    f.on_fan = true;
+  soil.get_humidity();
+	light.get_light();
+
+	control_temperature(air, heater, fan);
+	control_air_humidity(air, pump, fan);
+  control_soil_humidity(soil, pump, fan);
+	control_light(light, lamp);
+
+	heater.power();
+	fan.power();
+	lamp.power();
+	pump.power();
+
+if(millis() - lastUpdate >= 2000){
+  Serial.print(" влажность почвы "); Serial.print(soil.humidity);
+  Serial.print(" влажность воздуха "); Serial.print(air.humidity);
+  Serial.print(" температура "); Serial.print(air.temperature);
+  Serial.print(" освещенность "); Serial.println(light.light);
   }
-}
-
-void control_air_humidity(Humidity &hum, Pump &p, Fan&f){
-  if(hum.humidity > /**/ && hum.humidity < /**/){
-    p.on_pump = false;
-    f.on_fan = false;
-  } 
-  else if(hum.humidity < /**/){
-    p.on_pump = true;
-    f.on_fan = false;
-  }
-  else if(hum.humidity > /**/){
-    p.on_pump = false;
-    f.on_fan = true;
-  }
-}
-
-void control_soil_humidity(Humidity &hum, Pump &p, Fan&f){
-  if(hum.humidity > /**/ && hum.humidity < /**/){
-    p.on_pump = false;
-    f.on_fan = false;
-  } 
-  else if(hum.humidity < /**/){
-    p.on_pump = true;
-    f.on_fan = false;
-  }
-  else if(hum.humidity > /**/){
-    p.on_pump = false;
-    f.on_fan = true;
-  }
-}
-
-void control_light(Light &lig, Lamp&l){
-  if(lig.light < /**/){ //TODO границы
-    l.on_lamp = true;
-  }
-  else{
-    l.on_lamp = false;
-  }
-}
-
-
-Fan fan();
-Lamp lamp();
-Heater heater(); // TODO PIN
-Pump pump();
-
-Thermometer thermometer;
-Humidity airHumidity;
-Humidity soilHumidity;
-Light light;
-
-
-void setup(){
-
-}
-
-void loop() {
-		thermometer.get_temperature();
-		airHumidity.get_humidity();
-    soilHumidity.get_humidity();
-		light.get_light();
-
-		control_temperature(thermometer, heater, fan);
-		control_air_humidity(airHumidity, pump, fan);
-    control_soil_humidity(soilHumidity, pump, fan);
-		control_light(light, lamp);
-
-		heater.power();
-		fan.power();
-		lamp.power(); // TODO PIN
-		pump.power();
 }
